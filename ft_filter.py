1c9dad28180d7bd9fcfaf7fa13a3d4f855ceae7b
Mike Ireland <michael.ireland@anu.edu.au>
2015-12-14 13:49:01 +1100

    Changed the thresholding in this routine.

--- ft_filter.py
+++ ft_filter.py
@@ -4,7 +4,14 @@ import matplotlib.pyplot as plt
 import os
 import pylab as pl
 
-def ft_filter(grid_size=4):
+def ft_filter(grid_size=4,alpha=0.2):
+    """
+    Parameters
+    ----------
+    alpha: float
+        The minimum value of amplitude reduction calculated. "Wiggles" will get
+        1/alpha times bigger.
+    """
     # Read in the lightforge doc
     dir = os.path.dirname(__file__)
     filename = dir + "\\piaa_grid_" + str(grid_size) + ".dat"
@@ -23,7 +30,8 @@ def ft_filter(grid_size=4):
     #"at about 10 cycles/mm, amplitudes are reduced by a factor of 2"
     gauss_sigma = fov/0.1/2.35
     amp_reduction = np.exp(-(xy[0]**2 + xy[1]**2)/2.0/gauss_sigma**2)
-    amp_reduction = np.fft.fftshift( (amp_reduction + 0.1)/1.1 ) #Dodgy non divide by zero
+#    amp_reduction = np.fft.fftshift( (amp_reduction + 0.1)/1.1 ) #Dodgy non divide by zero
+    amp_reduction = np.fft.fftshift( np.maximum(amp_reduction,alpha) ) #Dodgy non divide by zero
     dd_filtered = np.real(np.fft.ifft2(np.fft.fft2(dd)/amp_reduction))
     
     pl.imshow(dd_filtered)
